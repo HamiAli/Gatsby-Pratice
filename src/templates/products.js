85a75ff9fded062d9c7429c28ed862f503031df7
HAmid <hamidprince823@gmail.com>
2021-12-18 12:16:12 +0500

    developed model

--- src/templates/products.js
+++ src/templates/products.js
@@ -1,11 +1,10 @@
 import React from 'react';
- import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 export default function Product({pageContext}) {
     console.log(pageContext)
     const {itemDetails} =pageContext
 return <div>
     <h1>{itemDetails.titlxe}</h1>
-    <p>{documentToReactComponents()}</p>
+    <p>BE</p>
     <h1>{itemDetails.slug}</h1>
 </div>
 }
