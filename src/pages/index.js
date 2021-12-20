34254e23c1143343fddc400ef01c21dc4be8d0d0
HAmid <hamidprince823@gmail.com>
2021-12-20 12:47:42 +0500

    Test gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,6 @@ import Layout from "../component/layout";
 
 export default function Home() {
     return <Layout>
-        <h1>This is Home page</h1>
+        <h1>This is Home page in gatsby</h1>
     </Layout>
 }
